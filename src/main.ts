6c5d32b1dc1c31f92af5e8b1ce20125f797e19e4
Gerardo Arceo <email@gerardoarceo.com>
2022-11-28 14:23:49 -0600

    changes

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   );
   await app.listen(process.env.APP_PORT);
   console.log(
-    `🎨 Lienzo Urbano server is ready on http://localhost:${process.env.APP_PORT}/graphql`,
+    `🚀🎨 Lienzo Urbano server is ready on http://localhost:${process.env.APP_PORT}/graphql`,
   );
 }
 bootstrap();
