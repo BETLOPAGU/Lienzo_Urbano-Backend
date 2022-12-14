86e3a83c6a90a3b72ae6c88dd32d41d6b2d40c9f
Gerardo Arceo <email@gerardoarceo.com>
2022-12-14 01:33:16 -0600

    added dist

--- dist/auth/guards/jwt-subscription.guard.d.ts
+++
@@ -1,7 +0,0 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-export declare class CatsGuard implements CanActivate {
-    readonly jwtService: JwtService;
-    constructor(jwtService: JwtService);
-    canActivate(context: ExecutionContext): boolean;
-}
